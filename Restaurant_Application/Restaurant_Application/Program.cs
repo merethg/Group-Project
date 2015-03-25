99ac37c7c8f348f6edf79dcd0c7a07e2f6c00da8
Dan-Williams93 <dannywiliams@hotmail.co.uk>
2015-03-25 12:26:34 +0000

    Addition of second form
    Table selection form added with ability to calculate database search
    value based on users input of diners

--- Restaurant_Application/Restaurant_Application/Program.cs
+++ Restaurant_Application/Restaurant_Application/Program.cs
@@ -15,7 +15,7 @@ namespace Restaurant_Application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmWelcome());
         }
     }
 }
---
+++ Restaurant_Application/Restaurant_Application/frmTableSelection.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Restaurant_Application
+{
+    partial class frmTableSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTableSelection));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblDiners = new System.Windows.Forms.Label();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblDiners
+            // 
+            this.lblDiners.AutoSize = true;
+            this.lblDiners.Location = new System.Drawing.Point(147, 26);
+            this.lblDiners.Name = "lblDiners";
+            this.lblDiners.Size = new System.Drawing.Size(46, 17);
+            this.lblDiners.TabIndex = 1;
+            this.lblDiners.Text = "label1";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(147, 68);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(46, 17);
+            this.lblSearch.TabIndex = 2;
+            this.lblSearch.Text = "label1";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(254, 302);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 3;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // frmTableSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 473);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblDiners);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmTableSelection";
+            this.Text = "frmTableSelection";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblDiners;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
