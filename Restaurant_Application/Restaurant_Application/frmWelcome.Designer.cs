d7ed31788310d528373fc20e62fea4fd579d146b
Dan-Williams93 <dannywiliams@hotmail.co.uk>
2015-03-26 15:38:51 +0000

    Table selection
    Allow user to select table from the graphical display altering the
    database for the selected table

--- Restaurant_Application/Restaurant_Application/frmWelcome.Designer.cs
+++ Restaurant_Application/Restaurant_Application/frmWelcome.Designer.cs
@@ -30,21 +30,6 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.btn1 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
-            this.btn9 = new System.Windows.Forms.Button();
-            this.btn8 = new System.Windows.Forms.Button();
-            this.btn7 = new System.Windows.Forms.Button();
-            this.btnClear = new System.Windows.Forms.Button();
-            this.btn0 = new System.Windows.Forms.Button();
-            this.btnBackSpace = new System.Windows.Forms.Button();
-            this.btnEnter = new System.Windows.Forms.Button();
-            this.txtDiners = new System.Windows.Forms.TextBox();
-            this.lblError = new System.Windows.Forms.Label();
             this.cmnOptions = new System.Windows.Forms.ComboBox();
             this.btnAccept = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -52,169 +37,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(275, 42);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.Size = new System.Drawing.Size(226, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Zizzi\'s";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(234, 84);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.Size = new System.Drawing.Size(397, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Please Enter the number of diners";
             // 
-            // btn1
-            // 
-            this.btn1.Location = new System.Drawing.Point(40, 228);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(48, 42);
-            this.btn1.TabIndex = 2;
-            this.btn1.Text = "1";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
-            // 
-            // btn2
-            // 
-            this.btn2.Location = new System.Drawing.Point(123, 228);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(48, 42);
-            this.btn2.TabIndex = 3;
-            this.btn2.Text = "2";
-            this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Click += new System.EventHandler(this.btn2_Click);
-            // 
-            // btn3
-            // 
-            this.btn3.Location = new System.Drawing.Point(194, 228);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(48, 42);
-            this.btn3.TabIndex = 4;
-            this.btn3.Text = "3";
-            this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn3_Click);
-            // 
-            // btn5
-            // 
-            this.btn5.Location = new System.Drawing.Point(123, 288);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(48, 42);
-            this.btn5.TabIndex = 5;
-            this.btn5.Text = "5";
-            this.btn5.UseVisualStyleBackColor = true;
-            this.btn5.Click += new System.EventHandler(this.btn5_Click);
-            // 
-            // btn4
-            // 
-            this.btn4.Location = new System.Drawing.Point(40, 288);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(48, 42);
-            this.btn4.TabIndex = 6;
-            this.btn4.Text = "4";
-            this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.btn4_Click);
-            // 
-            // btn6
-            // 
-            this.btn6.Location = new System.Drawing.Point(194, 288);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(48, 42);
-            this.btn6.TabIndex = 7;
-            this.btn6.Text = "6";
-            this.btn6.UseVisualStyleBackColor = true;
-            this.btn6.Click += new System.EventHandler(this.btn6_Click);
-            // 
-            // btn9
-            // 
-            this.btn9.Location = new System.Drawing.Point(194, 348);
-            this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(48, 42);
-            this.btn9.TabIndex = 8;
-            this.btn9.Text = "9";
-            this.btn9.UseVisualStyleBackColor = true;
-            this.btn9.Click += new System.EventHandler(this.btn9_Click);
-            // 
-            // btn8
-            // 
-            this.btn8.Location = new System.Drawing.Point(123, 348);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(48, 42);
-            this.btn8.TabIndex = 9;
-            this.btn8.Text = "8";
-            this.btn8.UseVisualStyleBackColor = true;
-            this.btn8.Click += new System.EventHandler(this.btn8_Click);
-            // 
-            // btn7
-            // 
-            this.btn7.Location = new System.Drawing.Point(40, 348);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(48, 42);
-            this.btn7.TabIndex = 10;
-            this.btn7.Text = "7";
-            this.btn7.UseVisualStyleBackColor = true;
-            this.btn7.Click += new System.EventHandler(this.btn7_Click);
-            // 
-            // btnClear
-            // 
-            this.btnClear.Location = new System.Drawing.Point(40, 410);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(61, 42);
-            this.btnClear.TabIndex = 11;
-            this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            // 
-            // btn0
-            // 
-            this.btn0.Location = new System.Drawing.Point(123, 410);
-            this.btn0.Name = "btn0";
-            this.btn0.Size = new System.Drawing.Size(48, 42);
-            this.btn0.TabIndex = 12;
-            this.btn0.Text = "0";
-            this.btn0.UseVisualStyleBackColor = true;
-            this.btn0.Click += new System.EventHandler(this.btn0_Click);
-            // 
-            // btnBackSpace
-            // 
-            this.btnBackSpace.Location = new System.Drawing.Point(182, 410);
-            this.btnBackSpace.Name = "btnBackSpace";
-            this.btnBackSpace.Size = new System.Drawing.Size(60, 42);
-            this.btnBackSpace.TabIndex = 13;
-            this.btnBackSpace.Text = "Delete";
-            this.btnBackSpace.UseVisualStyleBackColor = true;
-            this.btnBackSpace.Click += new System.EventHandler(this.btnBackSpace_Click);
-            // 
-            // btnEnter
-            // 
-            this.btnEnter.Location = new System.Drawing.Point(98, 486);
-            this.btnEnter.Name = "btnEnter";
-            this.btnEnter.Size = new System.Drawing.Size(105, 42);
-            this.btnEnter.TabIndex = 14;
-            this.btnEnter.Text = "Enter";
-            this.btnEnter.UseVisualStyleBackColor = true;
-            // 
-            // txtDiners
-            // 
-            this.txtDiners.Location = new System.Drawing.Point(98, 151);
-            this.txtDiners.Name = "txtDiners";
-            this.txtDiners.Size = new System.Drawing.Size(100, 22);
-            this.txtDiners.TabIndex = 15;
-            // 
-            // lblError
-            // 
-            this.lblError.AutoSize = true;
-            this.lblError.Location = new System.Drawing.Point(37, 187);
-            this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(242, 17);
-            this.lblError.TabIndex = 16;
-            this.lblError.Text = "Please input a valid number of diners";
-            this.lblError.Visible = false;
-            // 
             // cmnOptions
             // 
+            this.cmnOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmnOptions.FormattingEnabled = true;
             this.cmnOptions.Items.AddRange(new object[] {
             "1",
@@ -227,18 +69,19 @@
             "8",
             "9",
             "10"});
-            this.cmnOptions.Location = new System.Drawing.Point(368, 151);
+            this.cmnOptions.Location = new System.Drawing.Point(237, 176);
             this.cmnOptions.Name = "cmnOptions";
-            this.cmnOptions.Size = new System.Drawing.Size(193, 24);
+            this.cmnOptions.Size = new System.Drawing.Size(193, 37);
             this.cmnOptions.TabIndex = 17;
             this.cmnOptions.SelectedIndexChanged += new System.EventHandler(this.cmnOptions_SelectedIndexChanged);
             // 
             // btnAccept
             // 
             this.btnAccept.Enabled = false;
-            this.btnAccept.Location = new System.Drawing.Point(433, 367);
+            this.btnAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccept.Location = new System.Drawing.Point(248, 343);
             this.btnAccept.Name = "btnAccept";
-            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.Size = new System.Drawing.Size(182, 79);
             this.btnAccept.TabIndex = 18;
             this.btnAccept.Text = "Enter";
             this.btnAccept.UseVisualStyleBackColor = true;
@@ -251,27 +94,12 @@
             this.ClientSize = new System.Drawing.Size(711, 552);
             this.Controls.Add(this.btnAccept);
             this.Controls.Add(this.cmnOptions);
-            this.Controls.Add(this.lblError);
-            this.Controls.Add(this.txtDiners);
-            this.Controls.Add(this.btnEnter);
-            this.Controls.Add(this.btnBackSpace);
-            this.Controls.Add(this.btn0);
-            this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.btn7);
-            this.Controls.Add(this.btn8);
-            this.Controls.Add(this.btn9);
-            this.Controls.Add(this.btn6);
-            this.Controls.Add(this.btn4);
-            this.Controls.Add(this.btn5);
-            this.Controls.Add(this.btn3);
-            this.Controls.Add(this.btn2);
-            this.Controls.Add(this.btn1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmWelcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.frmWelcome_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -281,21 +109,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.Button btn3;
-        private System.Windows.Forms.Button btn5;
-        private System.Windows.Forms.Button btn4;
-        private System.Windows.Forms.Button btn6;
-        private System.Windows.Forms.Button btn9;
-        private System.Windows.Forms.Button btn8;
-        private System.Windows.Forms.Button btn7;
-        private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.Button btn0;
-        private System.Windows.Forms.Button btnBackSpace;
-        private System.Windows.Forms.Button btnEnter;
-        private System.Windows.Forms.TextBox txtDiners;
-        private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.ComboBox cmnOptions;
         private System.Windows.Forms.Button btnAccept;
     }
--- Restaurant_Application/Restaurant_Application/frmWelcome.cs
+++ Restaurant_Application/Restaurant_Application/frmWelcome.cs
@@ -11,92 +11,10 @@ namespace Restaurant_Application
 {
     public partial class frmWelcome : Form
     {
-        int numberOfDiners;
-
         public frmWelcome()
         {
             InitializeComponent();
         }
-
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "1";
-
-        }
-
-        private void btn2_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "2";
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "3";
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "4";
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "5";
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "6";
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "7";
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "8";
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "9";
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text += "0";
-        }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text = "";
-        }
-
-        private void btnBackSpace_Click(object sender, EventArgs e)
-        {
-            txtDiners.Text = txtDiners.Text.Remove(txtDiners.Text.Length - 1, 1);
-        }
-
-        //private void btnEnter_Click(object sender, EventArgs e)
-        //{
-        //    //Convert user input to integer
-        //    numberOfDiners = Convert.ToInt32(txtDiners.Text);
-
-        //    if (txtDiners.Text != "" && !txtDiners.Text.StartsWith("0"))
-        //    {            
-        //        //open table selection form and pass number of diners
-        //        frmTableSelection tb = new frmTableSelection(numberOfDiners);
-        //        tb.Show();
-        //        this.Hide();
-        //    }
-        //    else
-        //    {
-        //        lblError.Visible = true;
-        //        txtDiners.Text = "";
-        //    } 
-        //}
 
         private void cmnOptions_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -104,11 +22,6 @@ namespace Restaurant_Application
             {
                 btnAccept.Enabled = true;
             }
-        }
-
-        private void frmWelcome_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void btnAccept_Click(object sender, EventArgs e)
